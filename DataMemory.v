6825e005292fcdfaca4e3226cce2e237ed4d84e2
Mamdouh El Nakeeb <mamdouh.el.nakeeb@gmail.com>
2017-12-03 11:43:29 +0200

    files read and write feature

--- DataMemory.v
+++ DataMemory.v
@@ -1,4 +1,4 @@
-module DataMemory(clk, address, memRead, memWrite, dataIn, dateOut);
+module DataMemory(clk, address, memRead, memWrite, dataIn, dataOut);
 
     output reg [31:0] dataOut;
     
@@ -8,19 +8,31 @@ module DataMemory(clk, address, memRead, memWrite, dataIn, dateOut);
 
     input clk, memRead, memWrite;
 
+    reg [31:0] memory [0:127];
+
+    initial 
+    begin
+        // read from memory file
+        $readmemb("memory.txt", memory);
+    end
+
     always @(clk)
     begin
         if (memWrite == 1)
         begin
-
+            memory[address] <= dataIn;
         end
-
+        
         else if (memRead == 1)
         begin
-
+            dataOut <= memory[address];
         end
     end
 
-    
+    initial 
+    begin
+        // write in memory file
+        $writememb("memory.txt", memory);
+    end
 
 endmodule
